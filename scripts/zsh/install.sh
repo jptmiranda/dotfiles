793d0a6285acd62ee590442be8e9b758f24e8973
João Miranda <108836029+jptmiranda@users.noreply.github.com>
2024-03-26 22:32:04 +0000

    update zsh install.zsh

--- scripts/zsh/install.sh
+++ scripts/zsh/install.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+# ohmyzsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# zsh syntax highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/plugins/zsh-syntax-highlighting
+# catppuccin zsh syntax highlighting
 git clone https://github.com/catppuccin/zsh-syntax-highlighting.git
 mkdir ~/.zsh
 cp -v zsh-syntax-highlighting/themes/catppuccin_mocha-zsh-syntax-highlighting.zsh ~/.zsh/
 rm -rf zsh-syntax-highlighting
+# zsh autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/plugins/zsh-autosuggestions
